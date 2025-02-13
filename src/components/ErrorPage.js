12aa58d86726390e45986d5bf2c779648fb80d91
neeleshnainwal2001@gmail.com <neeleshnainwal2001gmail.com>
2025-02-13 18:50:15 +0530

    removed console.log

--- src/components/ErrorPage.js
+++ src/components/ErrorPage.js
@@ -3,7 +3,7 @@ import { useRouteError } from "react-router-dom";
 
 const ErrorPage = () => {
     const err=useRouteError()
-    console.log(err)
+    // console.log(err)
   return (
     <div>
       This is error page 
